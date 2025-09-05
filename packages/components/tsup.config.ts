fe6743035d529e45a4365c6fd2df34396037a20f
heshulei <heshulei@bytedance.com>
2025-09-05 20:08:45 +0800

    fix: :bug: 修复tsup

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from 'tsup'
 import esbuildPluginCssModules from 'esbuild-plugin-css-modules'
-import esbuildSassPlugin from 'esbuild-sass-plugin'
+import { sassPlugin } from 'esbuild-sass-plugin'
 
 export default defineConfig({
     entry: ['src/index.ts'],
     outDir: 'dist',
-    format: ['esm','cjs'],
+    format: ['esm', 'cjs'],
     dts: true,
     splitting: false,
     sourcemap: false,
     clean: true,
     minify: true,
-    esbuildPlugins: [
-        esbuildPluginCssModules(),
-        esbuildSassPlugin(),
-    ]
+    esbuildPlugins: [esbuildPluginCssModules(), sassPlugin()]
 })
